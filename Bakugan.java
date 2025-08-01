cf28b114684873bf0c91b7c40fb7863f6317614c
codificador529 <jrojoquezada@gmail.com>
2025-08-01 11:44:30 -0600

    segunda prueba de solucion de errores

--- Bakugan.java
+++ Bakugan.java
@@ -1,5 +1,5 @@
 public class Bakugan{
-    private String tipo;
-    private String atributo;
-    private int poderG;
+     String tipo;
+     String atributo;
+     int poderG;
 }
---
+++ Principal.java
@@ -0,0 +1,33 @@
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+
+        Bakugan ingram = new Bakugan();// instanciar un objeto
+        ingram.tipo = "Esfera";
+        ingram.atributo = "Ventus";
+        ingram.poderG = 450;
+        System.out.println(ingram);
+        System.out.println(ingram.tipo);
+        System.out.println(ingram.atributo);
+        System.out.println(ingram.poderG);
+
+        Bakugan drago = new Bakugan();
+        drago.tipo = "Esfera";
+        drago.atributo = "Pyrus";
+        drago.poderG = 460;
+        System.out.println(drago);
+        System.out.println(drago.tipo);
+        System.out.println(drago.atributo);
+        System.out.println(drago.poderG);
+
+        Bakugan percival = new Bakugan();
+        percival.tipo = "Esfera";
+        percival.atributo = "Darkus";
+        percival.poderG = 460;
+        System.out.println(percival.tipo);
+        System.out.println(percival.atributo);
+        System.out.println(percival.poderG);
+
+
+    }
+}
